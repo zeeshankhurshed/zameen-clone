43dd82da03f9fba742522f32a8ba47b27f57f806
zeeshankhurshed <109623408+zeeshankhurshed@users.noreply.github.com>
2022-10-12 17:19:53 +0500

    zameen project

--- js/main.js
+++ js/main.js
@@ -1,12 +1,52 @@
 function show(){
     let adit = document.getElementById('adit')
-  if (adit.style.transform == ('translateX(0)'))  {
-    adit.style.transform = ('translateX(-300)')
+  if (adit.style.transform == ('translateX(-300)'))  {
+    adit.style.transform = ('translateX(0)');
     }else {
-        adit.style.transform = ('translateX(0)');
+        adit.style.transform = ('translateX(-300)');
     }
   } 
+
+
   
+  function upShow(){
+    let boxUp= document.getElementById('boxUp')
+    if (boxUp.style.display == 'none') {
+        boxUp.style.display = 'block'
+    }else{
+        boxUp.style.display = 'none'
+    }
+  }
+  function upHide(){
+    let boxUp= document.getElementById('boxUp')
+    boxUp.style.display = 'none'
+  }
+
+  function rentShow(){
+    let boxRent= document.getElementById('boxRent')
+    if (boxRent.style.display == 'none') {
+        boxRent.style.display = 'block'
+    }else{
+        boxRent.style.display = 'none'
+    }
+  }
+  function rentHide(){
+    let boxRent= document.getElementById('boxRent')
+    boxRent.style.display = 'none'
+  }
+
+  function locationShow(){
+    let boxLocation= document.getElementById('boxLocation')
+    if (boxLocation.style.display == 'none') {
+        boxLocation.style.display = 'block'
+    }else{
+        boxLocation.style.display = 'none'
+    }
+  }
+//   function locationHide(){
+//     let boxRent= document.getElementById('boxRent')
+//     boxRent.style.display = 'none'
+//   }
 
 
 
